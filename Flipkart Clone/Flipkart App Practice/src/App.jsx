c00ac0a21a9a0678d324f42173d168908d9697ad
guptadivya5198 <guptadivya5198@gmail.com>
2024-09-03 11:38:39 +0530

    flipkart clone project

---
+++ Flipkart Clone/Flipkart App Practice/src/App.jsx
@@ -0,0 +1,15 @@
+import CardItems from './Pages/CardItems';
+import Navbar from './Pages/Navbar';
+import Footer from './services/Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <CardItems />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
