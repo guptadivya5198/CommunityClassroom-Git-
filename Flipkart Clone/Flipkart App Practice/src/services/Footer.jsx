e2c44b918a2b6ae443e546314a9311c2e1f395d5
guptadivya5198 <guptadivya5198@gmail.com>
2024-09-03 13:14:20 +0530

    modify

--- Flipkart Clone/Flipkart App Practice/src/services/Footer.jsx
+++ Flipkart Clone/Flipkart App Practice/src/services/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <div className="bg-gray-700">
       <div className="flex gap-2  text-white  justify-evenly">
         <div>
-          <p className="font-bold">About Us</p>
+          <p className="font-bold">ABOUT US</p>
           <div className="text-sm leading-5  ">
             <p className="hover:text-blue-400 hover:underline">Contact Us</p>
             <p className="hover:text-blue-400 hover:underline">Addres</p>
@@ -17,58 +17,40 @@ function Footer() {
           </div>
         </div>
         <div>
-          <p className="font-bold">About Us</p>
+          <p className="font-bold">GROUP COMPANIES</p>
           <div className="text-sm leading-5  ">
-            <p className="hover:text-blue-400 hover:underline">Contact Us</p>
-            <p className="hover:text-blue-400 hover:underline">Addres</p>
-            <p className="hover:text-blue-400 hover:underline">Mobile No.</p>
-            <p className="hover:text-blue-400 hover:underline">Lucknow</p>
-            <p className="hover:text-blue-400 hover:underline">FAQ</p>
-            <p className="hover:text-blue-400 hover:underline">Landmark</p>
+            <p className="hover:text-blue-400 hover:underline">Myntra</p>
+            <p className="hover:text-blue-400 hover:underline">Cleartrip</p>
+            <p className="hover:text-blue-400 hover:underline">Shopsy</p>
           </div>
         </div>
         <div>
-          <p className="font-bold">About Us</p>
+          <p className="font-bold">HELP</p>
           <div className="text-sm leading-5  ">
-            <p className="hover:text-blue-400 hover:underline">Contact Us</p>
-            <p className="hover:text-blue-400 hover:underline">Addres</p>
-            <p className="hover:text-blue-400 hover:underline">Mobile No.</p>
-            <p className="hover:text-blue-400 hover:underline">Lucknow</p>
+            <p className="hover:text-blue-400 hover:underline">Payments</p>
+            <p className="hover:text-blue-400 hover:underline">Shipping</p>
+            <p className="hover:text-blue-400 hover:underline">
+              Cancellation & Returns
+            </p>
             <p className="hover:text-blue-400 hover:underline">FAQ</p>
-            <p className="hover:text-blue-400 hover:underline">Landmark</p>
+            <p className="hover:text-blue-400 hover:underline">
+              Report Infringement
+            </p>
           </div>
         </div>
         <div>
-          <p className="font-bold">About Us</p>
+          <p className="font-bold">CONSUMER POLICY</p>
           <div className="text-sm leading-5  ">
-            <p className="hover:text-blue-400 hover:underline">Contact Us</p>
-            <p className="hover:text-blue-400 hover:underline">Addres</p>
-            <p className="hover:text-blue-400 hover:underline">Mobile No.</p>
-            <p className="hover:text-blue-400 hover:underline">Lucknow</p>
-            <p className="hover:text-blue-400 hover:underline">FAQ</p>
-            <p className="hover:text-blue-400 hover:underline">Landmark</p>
-          </div>
-        </div>
-        <div>
-          <p className="font-bold">About Us</p>
-          <div className="text-sm leading-5  ">
-            <p className="hover:text-blue-400 hover:underline">Contact Us</p>
-            <p className="hover:text-blue-400 hover:underline">Addres</p>
-            <p className="hover:text-blue-400 hover:underline">Mobile No.</p>
-            <p className="hover:text-blue-400 hover:underline">Lucknow</p>
-            <p className="hover:text-blue-400 hover:underline">FAQ</p>
-            <p className="hover:text-blue-400 hover:underline">Landmark</p>
-          </div>
-        </div>
-        <div>
-          <p className="font-bold">About Us</p>
-          <div className="text-sm leading-5  ">
-            <p className="hover:text-blue-400 hover:underline">Contact Us</p>
-            <p className="hover:text-blue-400 hover:underline">Addres</p>
-            <p className="hover:text-blue-400 hover:underline">Mobile No.</p>
-            <p className="hover:text-blue-400 hover:underline">Lucknow</p>
-            <p className="hover:text-blue-400 hover:underline">FAQ</p>
-            <p className="hover:text-blue-400 hover:underline">Landmark</p>
+            <p className="hover:text-blue-400 hover:underline">
+              Cancellation & Returns
+            </p>
+            <p className="hover:text-blue-400 hover:underline">Terms of Use</p>
+            <p className="hover:text-blue-400 hover:underline">Security</p>
+            <p className="hover:text-blue-400 hover:underline">Privacy</p>
+            <p className="hover:text-blue-400 hover:underline">Sitemap</p>
+            <p className="hover:text-blue-400 hover:underline">
+              EPR Compilance
+            </p>
           </div>
         </div>
       </div>
